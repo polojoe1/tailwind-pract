65c8c92d3ced46603872feba2326a1b1c7fee4de
Joseph Coats <coats.joseph30@gmail.com>
2022-11-15 23:32:16 -0600

    images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,8 @@ import Male from '../assets/paaa.png'
 import Perfect from '../assets/perfect.png'
 import Star from '../assets/star.png'
 import Black from '../assets/black.png'
+import LightGirl from '../assets/emma.png'
+import DarkGirl from '../assets/laquisha.png'
 const Hero = (props) => {
     const [users,setUsers]=useState([
         {name:"Paul",
@@ -19,13 +21,27 @@ const Hero = (props) => {
         active:"9 hours ago",
         perfect:false,
         female:false,
+        color:'dark'},
+        {name:"Emilia",
+        city:"Dallas, TX",
+        price:600,
+        active:"1 hour ago",
+        perfect:true,
+        female:true,
+        color:'light'},
+        {name:"Laquisha",
+        city:"Denton, TX",
+        price:900,
+        active:"3 hours ago",
+        perfect:false,
+        female:true,
         color:'dark'}
     ])
   return (
     <div className='pl-[10%] pr-[10%] w-fit flex '>
         {users.map((user,i)=>{
             return(<div className="flex rounded-sm shadow-md mr-6 hover:shadow-xl flex-col"><div key={i} className='topcard  relative'>
-                <img className='h-[150px] w-[225px] object-cover ' src={user.female?'':user.color==="dark"?Black:Male} alt='photo'/>
+                <img className='h-[150px] w-[225px] object-cover ' src={user.female?user.color==='dark'?DarkGirl:LightGirl:user.color==="dark"?Black:Male} alt='photo'/>
                 <img src={Star} alt="Star" className='absolute h-8 top-2 left-2 z-[2]' />
                 {user.perfect?<img className= 'absolute z-[2] w-[106px] top-0 right-0' src={Perfect} alt='Perfect'/>:''}
                 </div>
